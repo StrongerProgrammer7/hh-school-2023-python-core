1325790bbcfaa2bc5376f7cbe6235d754887d335
Abdyukov Zufar <swat55551@gmail.com>
2023-12-25 20:36:24 +0300

    Add some test for different range year

--- hw/TestMarket.py
+++ hw/TestMarket.py
@@ -51,5 +51,18 @@ class TestMarketMethods(unittest.TestCase):
 
         self.assertEqual(actual_result, expected_result)
 
+    def test_get_drinks_by_production_date_one_from_range(self):
+        print('test_get_drinks_by_production_date_one_from_range')
+        expected_result = [self.wines[1]]
+        actual_result = self.market.get_drinks_by_production_date(from_date=2022)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_get_drinks_by_production_date_one_to_range(self):
+        print('test_get_drinks_by_production_date_one_to_range')
+        expected_result = [self.wines[0],self.beers[0]]
+        actual_result = self.market.get_drinks_by_production_date(to_date=2019)
+
+        self.assertEqual(actual_result, expected_result)
 if __name__ == '__main__':
     unittest.main()
