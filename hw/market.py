179011a3d7c07abf87a39a62162d807ce7cb2fee
Abdyukov Zufar <swat55551@gmail.com>
2023-12-25 20:26:39 +0300

    Fix problem with get_drinks_by_production_date

--- hw/market.py
+++ hw/market.py
@@ -33,8 +33,9 @@ class Market:
     def __getListObjectsByDate(self,drinks: dict, from_date,to_date):
         list_drinks = list()
         for drink in drinks.values():
-            if Market._isBothDateExists(from_date, to_date) and Market._isDateDrinkInRange(drink, from_date, to_date):
-                list_drinks.append(drink)
+            if Market._isBothDateExists(from_date, to_date):
+                if Market._isDateDrinkInRange(drink, from_date, to_date):
+                    list_drinks.append(drink)
             elif from_date is not None and drink.production_date >= from_date:
                 list_drinks.append(drink)
             elif to_date is not None and drink.production_date <= to_date:
@@ -42,6 +43,14 @@ class Market:
         return list_drinks
 
     def get_drinks_by_production_date(self, from_date=None, to_date=None) -> list:
+        if from_date is None and to_date is None:
+            list_drinks = []
+            if self.wines is not None:
+                list_drinks += list(self.wines.values())
+            if self.beers is not None:
+                list_drinks += list(self.beers.values())
+            return list_drinks
+
         list_drinks = self.__getListObjectsByDate(self.wines,from_date,to_date)
         list_drinks += self.__getListObjectsByDate(self.beers,from_date,to_date)
         return list_drinks
