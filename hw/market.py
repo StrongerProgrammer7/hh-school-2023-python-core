5eb25e42aa9bd73997aaf0a7717bc05656525f46
Abdyukov Zufar <swat55551@gmail.com>
2023-12-25 20:36:06 +0300

    Refactor code for get list from dict

--- hw/market.py
+++ hw/market.py
@@ -21,16 +21,19 @@ class Market:
 
         return False
 
-    def get_drinks_sorted_by_title(self) -> list:
+    def __get_listDrinks(self,list_dict:list):
         list_drinks = list()
-        if self.wines is not None:
-            list_drinks += list(self.wines.values())
-        if self.beers is not None:
-            list_drinks += list(self.beers.values())
-        list_drinks = sorted(list_drinks, key=lambda x: x.title )
+        for dict_drink in list_dict:
+            if dict_drink is not None:
+                list_drinks += list(dict_drink.values())
+
+        return list_drinks
+    def get_drinks_sorted_by_title(self) -> list:
+        list_drinks = self.__get_listDrinks([self.wines,self.beers])
+        list_drinks = sorted(list_drinks, key=lambda x: x.title)
         return list_drinks
 
-    def __getListObjectsByDate(self,drinks: dict, from_date,to_date):
+    def __getListObjectsByDate(self, drinks: dict, from_date, to_date):
         list_drinks = list()
         for drink in drinks.values():
             if Market._isBothDateExists(from_date, to_date):
@@ -44,13 +47,8 @@ class Market:
 
     def get_drinks_by_production_date(self, from_date=None, to_date=None) -> list:
         if from_date is None and to_date is None:
-            list_drinks = []
-            if self.wines is not None:
-                list_drinks += list(self.wines.values())
-            if self.beers is not None:
-                list_drinks += list(self.beers.values())
-            return list_drinks
+            return  self.__get_listDrinks([self.wines,self.beers])
 
-        list_drinks = self.__getListObjectsByDate(self.wines,from_date,to_date)
-        list_drinks += self.__getListObjectsByDate(self.beers,from_date,to_date)
+        list_drinks = self.__getListObjectsByDate(self.wines, from_date, to_date)
+        list_drinks += self.__getListObjectsByDate(self.beers, from_date, to_date)
         return list_drinks
