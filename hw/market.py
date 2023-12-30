454606631c9dfa34355b867720d06bb3ea864d76
Abdyukov Zufar <swat55551@gmail.com>
2023-12-30 13:01:26 +0300

    Refactor file: Deleted not need check to the None create private method getDrinkByDate

--- hw/market.py
+++ hw/market.py
@@ -14,39 +14,42 @@ class Market:
 
     @printing_decorator
     def has_drink_with_title(self, title=None) -> bool:
-        if self.wines is not None:
-            if title in self.wines:
-                return True
-        if self.beers is not None:
-            if title in self.beers:
-                return True
+        if title in self.wines:
+            return True
+        if title in self.beers:
+            return True
 
         return False
 
     def __get_listDrinks(self,list_dict:list):
         list_drinks = list()
         for dict_drink in list_dict:
-            if dict_drink is not None:
-                list_drinks += list(dict_drink.values())
+            list_drinks += list(dict_drink.values())
 
         return list_drinks
 
-    @printing_decorator
+    #@printing_decorator
     def get_drinks_sorted_by_title(self) -> list:
         list_drinks = self.__get_listDrinks([self.wines,self.beers])
         list_drinks = sorted(list_drinks, key=lambda x: x.title)
         return list_drinks
 
+    def __getDrinkByDate(self,from_date,to_date,drink):
+        if self._isBothDateExists(from_date, to_date):
+            if self._isDateDrinkInRange(drink, from_date, to_date):
+                return drink
+        elif from_date is not None and drink.production_date >= from_date:
+            return drink
+        elif to_date is not None and drink.production_date <= to_date:
+            return drink
+        return None
+
     def __getListObjectsByDate(self, drinks: dict, from_date, to_date):
         list_drinks = list()
         for drink in drinks.values():
-            if Market._isBothDateExists(from_date, to_date):
-                if Market._isDateDrinkInRange(drink, from_date, to_date):
-                    list_drinks.append(drink)
-            elif from_date is not None and drink.production_date >= from_date:
-                list_drinks.append(drink)
-            elif to_date is not None and drink.production_date <= to_date:
-                list_drinks.append(drink)
+            drinkByDate = self.__getDrinkByDate(from_date,to_date,drink)
+            if drinkByDate is not None:
+                list_drinks.append(drinkByDate)
         return list_drinks
 
     @printing_decorator
